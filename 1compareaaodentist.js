636402135e6d3f8af32507ec61e1ee470383c80d
cinderella0302 <matthewliu0302@gmail.com>
2024-11-10 08:43:28 -0800

    feat: complete 1compareaaodentist.js

---
+++ 1compareaaodentist.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import TotalDentists from './models/dentists.model.js';
+import ADADentists from './models-ada/dentists.model.js';
+import AAODentists from './models-aao/dentists.model.js';
+
+dotenv.config();
+const mongooseURL = process.env.MONGODB_URL;
+
+await mongoose.connect(mongooseURL);
+console.log('Connected to mongodb');
+
+
+console.log('----- start at first');
+const cursor = await AAODentists.find().cursor();
+let flag = false;
+// console.log('----- start', cursor);
+try{
+    for(let doc = await cursor.next(); doc != null; doc = await cursor.next()){
+
+        const phoneNumber = doc.phone.slice(0, 5) + ' ' + doc.phone.slice(5);
+        // 17555, 21524
+        if(doc.id == '17555'){
+            flag = true;
+        }
+        console.log('------ start dentist: ', doc.id, phoneNumber);
+        if(flag){
+            const existed = await TotalDentists.findOne(
+                {Phone: phoneNumber}
+            );
+            // console.log('---- existed: ', existed);
+
+            if(existed){
+                await TotalDentists.findOneAndUpdate(
+                    { Phone: phoneNumber},
+                    {
+                        $set: {
+                            Source: "both",
+                            email: doc.email,
+                        }
+                    },
+                    { upsert: true }
+                );
+                console.log('---- update dentist: ', doc.id, phoneNumber);
+            }else{
+                await TotalDentists.create(
+                    {
+                        email: doc.email,
+                        Name: doc.store,
+                        Address: doc.address,
+                        Zip: doc.zip,
+                        City: doc.city,
+                        State: doc.state,
+                        Phone: phoneNumber,
+                        Website: doc.website,
+                        Source: "aao",
+                    }
+                );
+                console.log('---- create a new dentist: ', doc.id, phoneNumber);
+
+            }
+
+            // console.log('---- push dentist: ', addressId);
+        }else{
+            break;
+            // console.log('---- bypass dentist: ', doc.id);
+            // await mongoose.connection.close();
+        }
+    }
+}catch(error){
+    console.log('---- storeadadentist Error: ', error);
+}
+
+console.log('------------------ finish ');
+await mongoose.connection.close();
+console.log('Close the connection in mongoDB');
--- test.js
+++ test.js
@@ -1,17 +1,24 @@
-import Dentists from "./models/dentists.model.js";
-import Clinics from "./models/clinics.model.js";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import TotalDentists from './models/dentists.model.js';
+import ADADentists from './models-ada/dentists.model.js';
+import AAODentists from './models-aao/dentists.model.js';
 
 dotenv.config();
-const mongooseURL = process.env.MONGODB_URL_TEST;
+const mongooseURL = process.env.MONGODB_URL;
 
 await mongoose.connect(mongooseURL);
-console.log('Connected to mongoDB');
+console.log('Connected to mongodb');
 
-await Clinics.create({
-    Website: "test",
-    Dentists: []
-});
+console.log('----- start at first');
 
-console.log('saved');
+// console.log('----- start', cursor);
+const existed = await ADADentists.findOne(
+    // {Phone: "(703) 897-6453"}
+    { Phone: "(703) 640-1000"}
+);
+console.log('---- existed: ', existed);
+
+console.log('------------------ finish ');
+await mongoose.connection.close();
+console.log('Close the connection in mongoDB');
